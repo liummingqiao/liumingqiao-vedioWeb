eaf34f5027c6a8dc0902a492f198dabfafba507b
liumingqiao <50901715+liummingqiao@users.noreply.github.com>
2019-12-12 17:18:38 +0800

    utf-8

--- server/apps/admin/src/courses/courses.controller.ts
+++ server/apps/admin/src/courses/courses.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { InjectModel } from 'nestjs-typegoose';
+import { Course } from '@libs/db/model/course.model';
+ 
 
+@ApiTags('视频接口')
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    constructor(@InjectModel(Course)  private readonly model ){
+
+    }
+
+    @Get()
+    @ApiOperation({
+        summary: '获取全部视频列表'
+    })
+    async index(){
+     return  await  this.model.find()
+    }
+
+    @Post()
+    @ApiOperation({
+        summary : '增加视频'
+    })
+    async cerat(@B){
+        await 
+    }
+    
+
+}
